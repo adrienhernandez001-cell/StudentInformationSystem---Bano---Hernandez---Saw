957ad71fb777bc64c2a335fbb7561f409d922f8a
adrienhernandez001-cell <adrienhernandez001@gmail.com>
2026-07-26 00:50:58 +0800

    Added hard-coded login and dashboard

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentInformationSystem.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            if (username == "admin" && password == "admin123")
+            {
+                return RedirectToAction("Index", "Dashboard");
+            }
+
+            ViewBag.Error = "Invalid username or password.";
+
+            return View();
+        }
+    }
+}
